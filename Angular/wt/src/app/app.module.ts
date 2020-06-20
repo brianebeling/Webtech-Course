7bf1d1e095bed6a5640370100a309f1f4b5e9c36
LuxtraX <cornal.c@googlemail.com>
2020-06-20 16:16:58 +0200

    a

--- Angular/wt/src/app/app.module.ts
+++ Angular/wt/src/app/app.module.ts
@@ -2,27 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ButtonComponent } from './components/button/button.component';
-=======
 import { NewSeriesFormComponent } from './components/new-series-form/new-series-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> brian
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
     NavbarComponent,
-    ButtonComponent
-=======
+    ButtonComponent,
     NewSeriesFormComponent
->>>>>>> brian
+
   ],
   imports: [
     BrowserModule,
